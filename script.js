d32d16a5171b670828e6fe67e1f019cb92c7c954
Farman71 <farmanhussain71@gmail.com>
2025-03-06 14:57:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let cool=document.getElementById("colorSelect")
+let btn=document.getElementById("buton")
+btn.addEventListener("click",function(){
+	let selectedIndex=cool.selectedIndex
+		cool.remove(selectedIndex)	
+});
